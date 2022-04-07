73aa178fdf29e17fbd1829d5b3ef3c99c973bf4a
Timtonix <timtonic@outlook.fr>
2022-04-07 12:21:31 +0200

    use guard

--- lib/password_gen.ex
+++ lib/password_gen.ex
@@ -5,9 +5,16 @@ defmodule PasswordGen do
   def sommaire do
     IO.puts("Bienvenue dans le générateur de mots de passes")
     IO.puts("1.Générer un mot de passe\n2.Obtenir un hash")
-    IO.gets("\n>>Entrez votre choix\n>")
+    choice = String.to_integer(IO.getn("\n>>Entrez votre choix\n>"))
+    gen_passwd(choice)
+    IO.puts("Nope")
   end
-  def gen_passwd do
 
+  defp gen_passwd(number) when number == 1 do
+   IO.puts("Hello there")
+  end
+
+  defp gen_passwd(number) when number == 2 do
+    IO.puts("Noice")
   end
 end
