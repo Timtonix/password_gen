e2515c58f8d412e2982174ff5b47e3c52acc01f4
Timtonix <timtonic@outlook.fr>
2022-04-08 18:41:08 +0200

    Make that works

--- lib/password_gen.ex
+++ lib/password_gen.ex
@@ -3,18 +3,23 @@ defmodule PasswordGen do
   Documentation for `PasswordGen`.
   """
   @letter for n <- ?a..?z, do: << n :: utf8 >>
+  @capital for n <- ?A..?Z, do: << n :: utf8 >>
+  @digit for n <- ?0..?9, do: << n :: utf8 >>
+  @special for n <- [?@, ?#, ?$, ?%, ?[, ?], ?{, ?}], do: << n :: utf8 >>
+
   def sommaire do
     IO.puts("Bienvenue dans le générateur de mots de passes")
     IO.puts("1.Générer un mot de passe\n2.Obtenir un hash")
-    choice = IO.getn("\n>>Entrez votre choix\n>") |> String.trim()
-    |> String.to_integer()
+    choice = IO.gets("\n>>Entrez votre choix\n>")
+    {choice, _nothing} = Integer.parse(choice)
     redirect(choice)
   end
 
   defp redirect(number) when number == 1 do
-   strong = IO.getn(">Choisissez un niveau de sécurité : \n>1.Faible\n>2.Moyen\n>3.Fort\n>4.SuperFort\n>") |> String.trim()
-   |> String.to_integer()
-   gen_passwd(strong)
+   choice = IO.gets(">Choisissez un niveau de sécurité : \n>1.Fort\n>2.Très Fort\n>")
+   {choice, _nothing} = Integer.parse(choice)
+   gen_passwd(choice)
+
   end
   defp redirect(number) when number == 2 do
     IO.puts("Noice")
@@ -24,15 +29,12 @@ defmodule PasswordGen do
   end
 
   defp gen_passwd(number) when number == 1 do
-  IO.puts(@letter)
+    Process.sleep(1000)
+    IO.puts("Vous avez choisi le niveau fort")
+
   end
   defp gen_passwd(number) when number == 2 do
-
-  end
-  defp gen_passwd(number) when number == 3 do
-
-  end
-  defp gen_passwd(number) when number == 4 do
-
+    Process.sleep(1000)
+    IO.puts("Vous avez choisi le niveau très fort")
   end
 end
